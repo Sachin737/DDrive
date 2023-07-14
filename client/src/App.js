13271178e1efcd0ad4b55dfcb1e4cc1c00cc34bd
Sachin <89219332+Sachin737@users.noreply.github.com>
2023-07-14 11:48:39 +0530

    created my contract instance to interact with it using provider signer (metamask account) and contarctABI and deployed it on my local hardhat network

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import DDrive from "./artifacts/contracts/DDrive.sol/DDrive.json";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import myContractJSON from "./artifacts/contracts/DDrive.sol/DDrive.json";
 
 function App() {
+  const [contract, setContract] = useState();
+  const [provider, setProvider] = useState();
+  const [address, setAddress] = useState();
+
+  useEffect(() => {
+    const connectToContract = async () => {
+      const provider = new ethers.BrowserProvider(window.ethereum);
+
+      if (!provider) {
+        alert("Please connect your metamask account!");
+        return;
+      }
+
+      const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+      const contractABI = myContractJSON.abi;
+
+      const signer = await provider.getSigner();
+      const myContract = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+      );
+      setContract(myContract);
+      setProvider(provider);
+      setAddress(await signer.getAddress());
+
+      console.log(myContract);
+    };
+
+    connectToContract();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-[#323034] h-[100vh] flex justify-center items-center text-white">
+      <h1>Addres: {address ? address : "Connect using metamask"}</h1>
     </div>
   );
 }
